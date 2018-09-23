95c39c31509f702244c9daca90f711133d094449
uma0317 <blog3170@gmail.com>
2018-09-23 17:46:01 +0900

    mdファイルの数を数えれるようにした

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,11 @@
 use comrak::{markdown_to_html, ComrakOptions};
 
+use std::{fs, io};
 pub fn md_to_html(md: &str) -> String {
     markdown_to_html(md, &ComrakOptions::default())
+}
+
+pub fn count_md_files() -> io::Result<usize> {
+    let paths = fs::read_dir("posts")?;
+    return Ok(paths.count())
 }
--- tests/utils/mod.rs
+++ tests/utils/mod.rs
@@ -1,1 +1,12 @@
-mod md_to_html_test;
+use rustatic::utils;
+
+#[test]
+fn it_convert_md_to_html() {
+    assert_eq!(utils::md_to_html("Hello, **世界**!"),
+           "<p>Hello, <strong>世界</strong>!</p>\n");
+}
+
+#[test]
+fn it_count_md_files() {
+    assert_eq!(utils::count_md_files().unwrap(), 3);
+}
