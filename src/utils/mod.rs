15ce92b537fa3ee0b15db1cbf46403d1b161c19f
uma0317 <blog3170@gmail.com>
2018-09-24 03:39:46 +0900

    fix count_md_files()

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -5,17 +5,28 @@ use std::io;
 use std::io::Write;
 
 use {SOURCE_POSTS_PATH, GENERATED_POSTS_PATH};
-pub fn get_config() {
-
-}
 
 pub fn md_to_html(md: &str) -> String {
     markdown_to_html(md, &ComrakOptions::default())
 }
 
 pub fn count_md_files() -> io::Result<usize> {
-    let paths = fs::read_dir(SOURCE_POSTS_PATH)?;
-    return Ok(paths.count())
+    let mut count = 0;
+
+    for year_dir in fs::read_dir(SOURCE_POSTS_PATH)? {
+        let year_path = year_dir?.path();
+        for month_dir in fs::read_dir(year_path)? {
+            let month_path = month_dir?.path();
+            for day_dir in fs::read_dir(month_path)? {
+                let day_path = day_dir?.path();
+                let day_dir  = fs::read_dir(day_path)?;
+                
+                count += day_dir.count();
+            }
+        }
+    }
+
+    return Ok(count)
 }
 
 pub fn create_html_file(name: &str, contents: &str) -> io::Result<()>{
