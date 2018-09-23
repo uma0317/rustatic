6b3e4d665ab18d2a36354e8118298d291d8b37fa
uma0317 <blog3170@gmail.com>
2018-09-24 01:48:41 +0900

    htmlからmarkdownを生成できるようになった

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+#[macro_use]
 extern crate comrak;
+extern crate clap;
 
 pub mod utils;
+pub mod generate;
+pub mod cli;
+
+pub static SOURCE_POSTS_PATH: &'static str = "source/_posts";
+pub static GENERATED_POSTS_PATH: &'static str = "posts";
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 extern crate rustatic;
+extern crate clap;
 
-use rustatic::utils;
+use rustatic::{ utils, cli };
+use clap::{App, Arg, SubCommand};
 
 fn main() {
-    
+    let matches = cli::build().get_matches();
+
 }
